f9044ec18488457fa6e872c7e4a9414edbbb5456
lee <tiance.1984@gmail.com>
2017-02-20 18:19:19 +0800

    升级 项目结构

---
+++ config.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    # 基类 Config中包含通用配置，子类分别专用的配置。如果需要。可以添加其他的配置类。
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'My_bolg'
+    SQLALCHEMY_COMMIT_ON_TEARDOWM = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    # 开发配置子类
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = '587'
+    MAIL_USE_TLS = True  # SMTP 服务器好像只需要TLS协议
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # 千万不要把账户密码直接写入脚本,特别是准备开源的时候,为了保护账户信息,
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # 可以使用脚本从环境中导入敏感信息
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    # 测试配置子类
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    # 生成环境配置子类
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'prouduction': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
--- demo.py
+++ demo.py
@@ -1,11 +1,47 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 # 配置bootstrap是否使用本地的文件。
 app.config["BOOTSTRAP_SERVE_LOCAL"] = True
-bootstrap = Bootstrap(app)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/')
@@ -30,7 +66,7 @@ def music():
 
 @app.route('/about')
 def about():
-    return 'About'
+    return render_template('about.html', current_time=datetime.utcnow())
 
 
 @app.route('/post/')
@@ -43,4 +79,5 @@ def post_reproduced():
     pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
+#    app.run(debug=True)
+    manager.run()
